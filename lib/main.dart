0c93855e4db1f32083cc95214b7255cbf12b92da
KomorowskiKuba <komorii2k@gmail.com>
2021-05-14 19:30:53 +0200

    Added geolocation, added search field to add_place

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mobile_trip_planner/geolocation/app_bloc.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/homepage.dart';
 
@@ -14,18 +16,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Planowanie podrozy",
-      theme: ThemeData(
-        primaryColor: Color(0xff353942),
-        primaryColorLight: Color(0xff474e5e),
-        accentColor: Color(0xff0ebfeb),
-        hintColor: Color(0xff81e7fd),
-        backgroundColor: Color(0xff27282c),
-        errorColor: Colors.red
+    return ChangeNotifierProvider(
+      create: (context) => AppBloc(),
+      child: MaterialApp(
+        title: "Planowanie podrozy",
+        theme: ThemeData(
+          primaryColor: Color(0xff353942),
+          primaryColorLight: Color(0xff474e5e),
+          accentColor: Color(0xff0ebfeb),
+          hintColor: Color(0xff81e7fd),
+          backgroundColor: Color(0xff27282c),
+          errorColor: Colors.red
+        ),
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mobile_trip_planner/geolocation/app_bloc.dart';
 //import 'package:google_places_picker/google_places_picker.dart';
 
 import 'package:mobile_trip_planner/widgets/my_app_bar.dart';
+import 'package:provider/provider.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   @override
@@ -14,7 +16,6 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
@@ -22,13 +23,56 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final appBloc = Provider.of<AppBloc>(context);
+
     return Scaffold(
         appBar: MyAppBar.withoutIcons('Dodaj miejsce'),
         backgroundColor: Theme.of(context).primaryColor,
-        body: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(target: _center, zoom: 11),
-          onMapCreated: _onMapCreated,
+        body: Stack(
+          children: [
+            (appBloc.currentLocation == null)
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : GoogleMap(
+                    mapType: MapType.normal,
+                    myLocationEnabled: true,
+                    initialCameraPosition: CameraPosition(
+                        target: LatLng(appBloc.currentLocation.latitude,
+                            appBloc.currentLocation.longitude),
+                        zoom: 11),
+                    onMapCreated: _onMapCreated,
+                  ),
+            Positioned(
+                top: 10,
+                right: 10,
+                left: 10,
+                child: Container(
+                    child: Card(
+                  color: Theme.of(context).primaryColor,
+                  child: Row(
+                    children: [
+                      IconButton(
+                          color: Theme.of(context).accentColor,
+                          icon: Icon(Icons.search),
+                          onPressed: () {}),
+                      Expanded(
+                          child: TextField(
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                        cursorColor: Theme.of(context).accentColor,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.go,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
+                            hintText: 'Szukaj miejsca...'),
+                        onSubmitted: (val) {},
+                      ))
+                    ],
+                  ),
+                )))
+          ],
         ));
   }
 }
