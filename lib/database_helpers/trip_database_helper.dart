c8f8f6053e8d3e172d12fb854e0b8365893a3469
KomorowskiKuba <komorii2k@gmail.com>
2021-06-15 22:21:00 +0200

    Added basic trip database helper

--- lib/database_helpers/trip_database_helper.dart
+++ lib/database_helpers/trip_database_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:mobile_trip_planner/models/tripinfo_model.dart';
@@ -25,11 +27,72 @@ class TripDatabaseHelper {
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
-  Future _createDB(Database database, int version) async {}
+  Future _createDB(Database database, int version) async {
+    final String travelIdType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final String nameType = 'TEXT NOT NULL';
+    final String destinantionIdType = 'TEXT NOT NULL';
+    final String startDateType = 'TEXT NOT NULL';
+    final String endDateType = 'TEXT NOT NULL';
 
-  //Future<Tripinfo> create(Tripinfo tripinfo) async {
-  //  final database = await instance.database;
-  //final tripId = await database.insert(tableName, values);
+    await database.execute('''
+        CREATE TABLE $tableName (
+          ${TripinfoFields.travelId} $travelIdType,
+          ${TripinfoFields.name} $nameType,
+          ${TripinfoFields.destinantionId} $destinantionIdType,
+          ${TripinfoFields.startDate} $startDateType,
+          ${TripinfoFields.endDate} $endDateType
+        )
+      ''');
+  }
 
-  //}
+  Future<Tripinfo> create(Tripinfo tripinfo) async {
+    final database = await instance.database;
+    final travelId = await database.insert(tableName, tripinfo.toJson());
+
+    return tripinfo.copy(travelId: travelId);
+  }
+
+  Future<Tripinfo> readTripinfo(int travelId) async {
+    final database = await instance.database;
+
+    final maps = await database.query(tableName,
+        columns: TripinfoFields.values,
+        where: '$TripinfoFields.travelId = ?',
+        whereArgs: [travelId]);
+
+    if (maps.isNotEmpty) {
+      return Tripinfo.fromJson(maps.first);
+    } else {
+      throw Exception('Id not found!');
+    }
+  }
+
+  Future<List<Tripinfo>> readlAllInfo() async {
+    final database = await instance.database;
+
+    final result = await database.query(tableName);
+
+    return result.map((json) => Tripinfo.fromJson(json)).toList();
+  }
+
+  Future<int> ipdate(Tripinfo tripinfo) async {
+    final database = await instance.database;
+
+    return database.update(tableName, tripinfo.toJson(),
+        where: '${TripinfoFields.travelId} = ?',
+        whereArgs: [tripinfo.travelId]);
+  }
+
+  Future<int> delete(int? travelId) async {
+    final database = await instance.database;
+
+    return await database.delete(tableName,
+        where: '${TripinfoFields.travelId} = ?', whereArgs: [travelId]);
+  }
+
+  Future close() async {
+    final database = await instance.database;
+
+    database.close();
+  }
 }
--- lib/models/tripinfo_model.dart
+++ lib/models/tripinfo_model.dart
@@ -1,14 +1,68 @@
+final String tableName = 'tripinfo_database';
+
+class TripinfoFields {
+  static final List<String> values = [
+    travelId,
+    name,
+    destinantionId,
+    startDate,
+    endDate
+  ];
+
+  static final String travelId = 'travelId';
+  static final String name = 'name';
+  static final String destinantionId = 'destinationId';
+  static final String startDate = 'startDate';
+  static final String endDate = 'endDate';
+}
+
+//TODO: DODAC POLE NA SCIEZKE DO ZDJECIA
+
 class Tripinfo {
-  int tripId;
+  int? travelId;
   String name;
   String destinationId;
   DateTime startDate;
   DateTime endDate;
 
   Tripinfo(
-      {required this.tripId,
+      {this.travelId,
       required this.name,
       required this.destinationId,
       required this.startDate,
       required this.endDate});
+
+  static Tripinfo fromJson(Map<String, dynamic> json) => Tripinfo(
+      travelId: json[TripinfoFields.travelId] as int?,
+      name: json[TripinfoFields.name] as String,
+      destinationId: json[TripinfoFields.destinantionId] as String,
+      startDate: json[TripinfoFields.startDate] as DateTime,
+      endDate: json[TripinfoFields.endDate] as DateTime);
+
+  Map<String, Object?> toJson() => {
+        TripinfoFields.travelId: travelId,
+        TripinfoFields.name: name,
+        TripinfoFields.destinantionId: destinationId,
+        TripinfoFields.startDate: startDate,
+        TripinfoFields.endDate: endDate
+      };
+
+  Tripinfo copy({
+    int? travelId,
+    String? name,
+    String? destinationId,
+    DateTime? startDate,
+    DateTime? endDate,
+  }) =>
+      Tripinfo(
+          travelId: travelId ?? this.travelId,
+          name: name ?? this.name,
+          destinationId: destinationId ?? this.destinationId,
+          startDate: startDate ?? this.startDate,
+          endDate: endDate ?? this.endDate);
+
+  @override
+  String toString() {
+    return super.toString();
+  }
 }
