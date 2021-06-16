187201717aa0782fae67f5124bc638c6455e9fed
KomorowskiKuba <komorii2k@gmail.com>
2021-06-16 15:41:02 +0200

    Fixed issues with app closing

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -66,126 +66,133 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      appBar: MyAppBar('Witaj, ' + _name + '!', Icon(Icons.settings), () {
-        Navigator.push(context,
-                MaterialPageRoute(builder: (context) => SettingsScreen()))
-            .then((value) => _loadName());
-      }),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 5,
-            ),
-            Center(
-              child: Container(
-                width: double.infinity,
-                child: Card(
-                  color: Theme.of(context).primaryColor,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    child: Column(
-                      children: [
-                        Container(
-                          alignment: Alignment.topLeft,
-                          child: Text('Porada na dziś',
-                              style: TextStyle(
-                                color: Theme.of(context).accentColor,
-                                fontSize: 35,
-                              )),
-                        ),
-                        FutureBuilder(
-                            future: _future,
-                            builder: (context, AsyncSnapshot snapshot) {
-                              if (!snapshot.hasData) {
-                                return Text('Ładowanie...');
-                              }
+    return WillPopScope(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        appBar: MyAppBar('Witaj, ' + _name + '!', Icon(Icons.settings), () {
+          Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SettingsScreen()))
+              .then((value) => _loadName());
+        }),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 5,
+              ),
+              Center(
+                child: Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Theme.of(context).primaryColor,
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        children: [
+                          Container(
+                            alignment: Alignment.topLeft,
+                            child: Text('Porada na dziś',
+                                style: TextStyle(
+                                  color: Theme.of(context).accentColor,
+                                  fontSize: 35,
+                                )),
+                          ),
+                          FutureBuilder(
+                              future: _future,
+                              builder: (context, AsyncSnapshot snapshot) {
+                                if (!snapshot.hasData) {
+                                  return Text('Ładowanie...');
+                                }
 
-                              List<dynamic> parsedJson =
-                                  jsonDecode(snapshot.data);
-                              _tips = parsedJson.map((element) {
-                                return Tip(element.toString());
-                              }).toList();
+                                List<dynamic> parsedJson =
+                                    jsonDecode(snapshot.data);
+                                _tips = parsedJson.map((element) {
+                                  return Tip(element.toString());
+                                }).toList();
 
-                              return Text(
-                                  _tips.length == 0
-                                      ? "Ładowanie..."
-                                      : _tips[_getRandomIndex(_tips.length)]
-                                          .toString(),
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 28,
-                                  ));
-                            })
-                      ],
+                                return Text(
+                                    _tips.length == 0
+                                        ? "Ładowanie..."
+                                        : _tips[_getRandomIndex(_tips.length)]
+                                            .toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 28,
+                                    ));
+                              })
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Center(
-                child: NextScreenTile(
-                    title: 'Zaplanuj podróż',
-                    icon: Icon(
-                      Icons.airplanemode_on_sharp,
-                      size: 40,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    function: () {
-                      Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => TripPlanScreen()))
-                          .whenComplete(loadTravels);
-                    })),
-            isLoading
-                ? CircularProgressIndicator()
-                : Center(
-                    child: Container(
-                    width: double.infinity,
-                    child: Card(
-                      color: Theme.of(context).primaryColor,
+              Center(
+                  child: NextScreenTile(
+                      title: 'Zaplanuj podróż',
+                      icon: Icon(
+                        Icons.airplanemode_on_sharp,
+                        size: 40,
+                        color: Theme.of(context).accentColor,
+                      ),
+                      function: () {
+                        Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => TripPlanScreen()))
+                            .whenComplete(loadTravels);
+                      })),
+              isLoading
+                  ? CircularProgressIndicator()
+                  : Center(
                       child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                //Icon(),
-                                Text('Poprzednie podróże',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                    ))
-                              ],
-                            ),
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Row(
+                      width: double.infinity,
+                      child: Card(
+                        color: Theme.of(context).primaryColor,
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: Column(
+                            children: [
+                              Row(
                                 children: [
-                                  for (var currentTravel in _travels)
-                                    TripPreviewTile(currentTravel, () {
-                                      Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      TravelManageScreen(
-                                                          currentTravel)))
-                                          .then((val) => loadTravels());
-                                    }),
+                                  //Icon(),
+                                  Text('Poprzednie podróże',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                      ))
                                 ],
                               ),
-                            )
-                          ],
+                              SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Row(
+                                  children: [
+                                    for (var currentTravel in _travels)
+                                      TripPreviewTile(currentTravel, () {
+                                        Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        TravelManageScreen(
+                                                            currentTravel)))
+                                            .then((val) => loadTravels());
+                                      }),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ))
-          ],
+                    ))
+            ],
+          ),
         ),
       ),
+      onWillPop: () {
+        SystemNavigator.pop();
+        Navigator.of(context).pop(true);
+        return Future.value();
+      },
     );
   }
 }
